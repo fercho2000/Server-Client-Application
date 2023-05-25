302ee021d545ff4676f4887ecf357c91fa2edb86
CEIBA\andres.orozco <andres.orozco@ceiba.com.co>
2023-05-24 20:55:00 -0500

    Se ajusta el nombre del host en la aplicación del cliente

--- socket_application/src/main/java/org/example/SocketClientExample.java
+++ socket_application/src/main/java/org/example/SocketClientExample.java
@@ -3,18 +3,16 @@ package org.example;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class SocketClientExample {
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
-        InetAddress host = InetAddress.getLocalHost();
         Socket socket = null;
         ObjectOutputStream oos = null;
         ObjectInputStream ois = null;
         for(int i=0; i<5;i++){
-            socket = new Socket(host.getHostName(), 9876);
+            socket = new Socket("server", 9876);
             oos = new ObjectOutputStream(socket.getOutputStream());
             System.out.println("Sending request to Socket Server");
             if(i==4)oos.writeObject("exit");
