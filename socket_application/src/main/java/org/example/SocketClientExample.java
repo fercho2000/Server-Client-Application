b566aa5963db38fd37c315b10ff104a2519b4776
Jose Fernando U <44246337+fercho2000@users.noreply.github.com>
2023-05-24 21:02:28 -0500

    GRAL: Se actualiza documentación del código

--- socket_application/src/main/java/org/example/SocketClientExample.java
+++ socket_application/src/main/java/org/example/SocketClientExample.java
@@ -4,24 +4,46 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class SocketClientExample {
-    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
-        Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        for(int i=0; i<5;i++){
-            socket = new Socket("server", 9876);
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String host = "server"; // Dirección del servidor
+        Socket socket = null; // Socket para la comunicación
+        ObjectOutputStream oos = null; // Stream de salida de objetos
+        ObjectInputStream ois = null; // Stream de entrada de objetos
+
+        // Bucle para enviar y recibir solicitudes al servidor
+        for (int i = 0; i < 5; i++) {
+            // Establecer una conexión con el servidor
+            socket = new Socket(host, 9876);
+
+            // Obtener el stream de salida de objetos del socket
             oos = new ObjectOutputStream(socket.getOutputStream());
+
             System.out.println("Sending request to Socket Server");
-            if(i==4)oos.writeObject("exit");
-            else oos.writeObject(""+i);
+
+            // Enviar una solicitud al servidor
+            if (i == 4) {
+                oos.writeObject("exit");
+            } else {
+                oos.writeObject("" + i);
+            }
+
+            // Obtener el stream de entrada de objetos del socket
             ois = new ObjectInputStream(socket.getInputStream());
+
+            // Leer la respuesta del servidor
             String message = (String) ois.readObject();
+
             System.out.println("Message: " + message);
+
+            // Cerrar los streams y el socket
             ois.close();
             oos.close();
+            socket.close();
+
+            // Pausar el hilo actual durante 100 milisegundos
             Thread.sleep(100);
         }
     }
--- socket_server/src/main/java/org/example/SocketServerExample.java
+++ socket_server/src/main/java/org/example/SocketServerExample.java
@@ -7,24 +7,43 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServerExample {
-    private static ServerSocket server;
-    private static int port = 9876;
+    private static ServerSocket server; // Socket del servidor
+    private static int port = 9876; // Puerto de escucha del servidor
 
-    public static void main(String args[]) throws IOException, ClassNotFoundException{
-        server = new ServerSocket(port);
-        while(true){
+    public static void main(String args[]) throws IOException, ClassNotFoundException {
+        server = new ServerSocket(port); // Crear un ServerSocket en el puerto especificado
+
+        // Bucle para esperar solicitudes de clientes
+        while (true) {
             System.out.println("Waiting for the client request");
+
+            // Aceptar una conexión entrante del cliente
             Socket socket = server.accept();
+
+            // Obtener el stream de entrada de objetos del socket
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+            // Leer el mensaje enviado por el cliente
             String message = (String) ois.readObject();
+
             System.out.println("Message Received: " + message);
+
+            // Obtener el stream de salida de objetos del socket
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            oos.writeObject("Hi Client "+message);
+
+            // Enviar una respuesta al cliente
+            oos.writeObject("Hi Client " + message);
+
+            // Cerrar los streams y el socket
             ois.close();
             oos.close();
             socket.close();
-            if(message.equalsIgnoreCase("exit")) break;
+
+            // Comprobar si el mensaje es "exit" para finalizar el servidor
+            if (message.equalsIgnoreCase("exit"))
+                break;
         }
+
         System.out.println("Shutting down Socket server!!");
         server.close();
     }
